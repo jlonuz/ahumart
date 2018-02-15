25b93013296f7ceeda56c1dce4456ed53759d378
J.Lonuz <lonuz.j@gmail.com>
2018-02-14 21:03:23 -0300

    add tienda and contact page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
-$("[data-fancybox]").fancybox({
+$(document).ready(function(){
+//   // $('.slider-home').slick({
+//   //   dots: true
+//   // });
+  $("[data-fancybox]").fancybox({
     // Options will go here
   });
+});
+
+
+function initMap() {
+  var ahumart = {lat: -45.5721488, lng: -72.066253};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: ahumart
+  });
+  var marker = new google.maps.Marker({
+    position: ahumart,
+    map: map
+  });
+}
